40b1cfedbc3223da96caf941dd44074e23cd229a
DesiCode123 <sharmila@live.no>
2023-09-12 14:25:44 +0200

    printf to formatting string

---
+++ src/StarsPractice/uke1/FormatOutput.java
@@ -0,0 +1,9 @@
+package StarsPractice.uke1;
+//formatting output using printf
+public class FormatOutput {
+    public static void main(String[] args) {
+        System.out.printf("%10s","hello"); //
+
+    }
+
+}

6c5e1b5834c3823e283307e87cff61a5b0b37fa8
DesiCode123 <sharmila@live.no>
2023-09-16 22:08:39 +0200

    Algorithum Tournement winner

--- src/StarsPractice/Polymorf/TournamentWinner.java
+++ src/StarsPractice/Polymorf/TournamentWinner.java
@@ -1,17 +1,15 @@
 package StarsPractice.Polymorf;
-
 import java.util.*;
 
-class TournamenWinner {
+class TournamentWinner {
 
-    public int HOME_TEAM_WON = 1;
+    public static final int HOME_TEAM_WON = 1;
 
     public String tournamentWinner(
             ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results
     ) {
         String currentBestTeam = "";
-        HashMap<String,Integer> scores = new HashMap<String,Integer>();
-        scores.put(currentBestTeam,0);
+        HashMap<String, Integer> scores = new HashMap<>();
 
         for(int idx = 0; idx < competitions.size(); idx++){
             ArrayList<String> competition = competitions.get(idx);
@@ -20,24 +18,51 @@ class TournamenWinner {
             String homeTeam = competition.get(0);
             String awayTeam = competition.get(1);
 
-            String winningTeam = (result == HOME_TEAM_WON)? homeTeam : awayTeam;
-            updateSCores(winningTeam, 3, scores);
+            String winningTeam = (result == HOME_TEAM_WON) ? homeTeam : awayTeam;
+            updateScores(winningTeam, 3, scores);
 
-            if(scores.get(winningTeam) > scores.get(currentBestTeam)){
+            if(!scores.containsKey(currentBestTeam) || scores.get(winningTeam) > scores.get(currentBestTeam)){
                 currentBestTeam = winningTeam;
             }
         }
         return currentBestTeam;
     }
-    public void updateSCores(String team, int points, HashMap<String,Integer> scores){
-        if(!scores.containsKey(team)){
-            scores.put(team,0);
-        }
 
-        scores.put(team,scores.get(team) + points);
+    public void updateScores(String team, int points, HashMap<String, Integer> scores){
+        scores.put(team, scores.getOrDefault(team, 0) + points);
+    }
 
+    public static void main(String[] args) {
+        TournamentWinner p = new TournamentWinner();
+
+        ArrayList<ArrayList<String>> input1 = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(Arrays.asList("HTML", "Java")),
+                        new ArrayList<>(Arrays.asList("Java", "Python")),
+                        new ArrayList<>(Arrays.asList("Python", "HTML"))
+                )
+        );
+
+        ArrayList<Integer> results1 = new ArrayList<>(Arrays.asList(0, 1, 1));
+
+        ArrayList<ArrayList<String>> input2 = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(Arrays.asList("HTML", "Java")),
+                        new ArrayList<>(Arrays.asList("Java", "Python")),
+                        new ArrayList<>(Arrays.asList("Python", "HTML")),
+                        new ArrayList<>(Arrays.asList("C#", "Python")),
+                        new ArrayList<>(Arrays.asList("Java", "C#")),
+                        new ArrayList<>(Arrays.asList("C#", "HTML"))
+                )
+        );
+
+        ArrayList<Integer> results2 = new ArrayList<>(Arrays.asList(0, 1, 1, 1, 0, 1));
+
+        System.out.println(p.tournamentWinner(input1, results1));  // Forventet output: Java
+        System.out.println(p.tournamentWinner(input2, results2));  // Forventet output: C#
     }
 }
+
 
 /*{
   "competitions": [
@@ -59,3 +84,5 @@ Test Case 3
   ],
   "results": [0, 1, 1, 1, 0, 1]
 }
+
+ */

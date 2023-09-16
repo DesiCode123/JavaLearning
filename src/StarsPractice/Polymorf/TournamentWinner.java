0111c175ee91908dffaecba92f8f7bab63f93940
DesiCode123 <sharmila@live.no>
2023-09-16 22:02:40 +0200

    Algorithum Tournement winner

--- src/StarsPractice/Polymorf/TournamentWinner.java
+++ src/StarsPractice/Polymorf/TournamentWinner.java
@@ -1,4 +1,41 @@
 package StarsPractice.Polymorf;
 
-public class TournamentWinner {
+import java.util.*;
+
+class TournamenWinner {
+
+    public int HOME_TEAM_WON = 1;
+
+    public String tournamentWinner(
+            ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results
+    ) {
+        String currentBestTeam = "";
+        HashMap<String,Integer> scores = new HashMap<String,Integer>();
+        scores.put(currentBestTeam,0);
+
+        for(int idx = 0; idx < competitions.size(); idx++){
+            ArrayList<String> competition = competitions.get(idx);
+            int result = results.get(idx);
+
+            String homeTeam = competition.get(0);
+            String awayTeam = competition.get(1);
+
+            String winningTeam = (result == HOME_TEAM_WON)? homeTeam : awayTeam;
+            updateSCores(winningTeam, 3, scores);
+
+            if(scores.get(winningTeam) > scores.get(currentBestTeam)){
+                currentBestTeam = winningTeam;
+            }
+        }
+        return currentBestTeam;
+    }
+    public void updateSCores(String team, int points, HashMap<String,Integer> scores){
+        if(!scores.containsKey(team)){
+            scores.put(team,0);
+        }
+
+        scores.put(team,scores.get(team) + points);
+
+    }
 }
+
